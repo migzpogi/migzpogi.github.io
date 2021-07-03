8b5e9220c80d830f92c340e9b2b8fa83fd07426d
Migz Estrella <migz.zgim@gmail.com>
2021-07-03 23:03:33 +0800

    Build site

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -14,21 +14,20 @@ import { smoothLoading } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi
 import { tocHighlight } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toc-highlight.js';tocHighlight();
 import { postNavSearch } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { copyLineLinks } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/code/line-links/copy-line-link.js';copyLineLinks();
-import { reloadOnChange } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/serve/reload.js';reloadOnChange();
 import { CollapseControl } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/collapse/collapse-control.js';
 import { ToCToggle } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/darkmode/index.js';
 import { ConfigTransport } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/transport/config.js';
-import { ToCPrevNext } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/prevnext/index.js';
 import { TabSelector } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/tabs/selector.js';
+import { ToCPrevNext } from 'C:/Users/Migz-NZXT/Documents/gitprojects/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es5/components/page/toc/prevnext/index.js';
 
 const components = {
   'Ifg8pDJj2CBZ3Z8umrOgDg==': CollapseControl,
   'Nh6re0dgnQiGVEdDYkhqmw==': ToCToggle,
   'DirrDhSuHWw/BdHCBv7rXQ==': DarkModeSwitch,
   'cENvn186VTFomXH41rxoNg==': ConfigTransport,
-  'Nm2TKrJuxpRyhNfFQiRtjg==': ToCPrevNext,
-  '4gB8PPOMhTAsadzFtD6Puw==': TabSelector
+  '4gB8PPOMhTAsadzFtD6Puw==': TabSelector,
+  'Nm2TKrJuxpRyhNfFQiRtjg==': ToCPrevNext
 };
 
 const renderer = getRenderer();
