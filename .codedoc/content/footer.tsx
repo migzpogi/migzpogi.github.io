771342c1b76cc724fe1e87e6899c2a4987535a6d
migzpogi <migz.zgim@gmail.com>
2021-10-11 09:22:06 +0800

    Modifies footer

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -3,17 +3,24 @@ import { Footer as _Footer, GitterToggle$, Watermark} from '@codedoc/core/compon
 
 
 export function Footer(config: CodedocConfig, renderer: any) {
-  let github$;
-  if (config.misc?.github)
-    github$ = <a href={`https://github.com/${config.misc.github.user}/${config.misc.github.repo}/`} 
-                target="_blank">GitHub</a>;
+  // let github$;
+  // if (config.misc?.github)
+  //   github$ = <a href={`https://github.com/${config.misc.github.user}/${config.misc.github.repo}/`} 
+  //               target="_blank">GitHub</a>;
 
-  let community$;
-  if (config.misc?.gitter)
-    community$ = <GitterToggle$ room={config.misc.gitter.room}/>
+  // let community$;
+  // if (config.misc?.gitter)
+  //   community$ = <GitterToggle$ room={config.misc.gitter.room}/>
 
-  if (github$ && community$) return <_Footer>{github$}<hr/>{community$}</_Footer>;
-  else if (github$) return <_Footer>{github$}</_Footer>;
-  else if (community$) return <_Footer>{community$}</_Footer>;
-  else return <_Footer><Watermark/></_Footer>;
+  // if (github$ && community$) return <_Footer>{github$}<hr/>{community$}</_Footer>;
+  // else if (github$) return <_Footer>{github$}</_Footer>;
+  // else if (community$) return <_Footer>{community$}</_Footer>;
+  // else return <_Footer><Watermark/></_Footer>;
+  return <_Footer>
+  <a href="https://github.com/migzpogi">GitHub</a>
+  <hr/>
+  <a href="https://www.linkedin.com/in/migzestrella/">LinkedIn</a>
+  <hr/>
+  <a href="https://github.com/CONNECT-platform/codedoc/">Codedoc</a>
+</_Footer>;
 }
