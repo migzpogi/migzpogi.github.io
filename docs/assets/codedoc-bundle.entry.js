28f15a555ba996505e12e7cf5f9487b1518a409f
migzpogi <migz.zgim@gmail.com>
2022-09-30 23:34:59 +0200

    Adds blog link in footer

--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -15,20 +15,21 @@ import { tocHighlight } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.
 import { postNavSearch } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
 import { copyLineLinks } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
 import { gatherFootnotes } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { reloadOnChange } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/serve/reload.js';reloadOnChange();
 import { ToCToggle } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { TabSelector } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
 import { CollapseControl } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/collapse/collapse-control.js';
 import { ToCPrevNext } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { TabSelector } from '/home/migzlegionubuntu/ubuntu_git/migzpogi.github.io/.codedoc/node_modules/@codedoc/core/dist/es6/components/tabs/selector.js';
 
 const components = {
   '2KQ/U4adi/Z/oAWMjb12/g==': ToCToggle,
   'jR4pIl18XyJlqy0MRQ030Q==': DarkModeSwitch,
   'aa09u7XPvNn0PoGLSC9eYA==': ConfigTransport,
-  'wbEYsoiy+k0DlDA91YBqcg==': TabSelector,
   'Nhhl4b58B/csvm7vrQnzrg==': CollapseControl,
-  'u57FALME0msFjeOPW2+9hg==': ToCPrevNext
+  'u57FALME0msFjeOPW2+9hg==': ToCPrevNext,
+  'wbEYsoiy+k0DlDA91YBqcg==': TabSelector
 };
 
 const renderer = getRenderer();
