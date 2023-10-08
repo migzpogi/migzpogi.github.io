63257b0b3c0b2d54a698d2b1f51d1a79aca02ec2
migzpogi <migz.zgim@gmail.com>
2023-10-08 22:35:31 +0200

    Edits footer links

--- .codedoc/content/footer.tsx
+++ .codedoc/content/footer.tsx
@@ -3,17 +3,11 @@ import { Footer as _Footer, GitterToggle$, Watermark} from '@codedoc/core/compon
 
 
 export function Footer(config: CodedocConfig, renderer: any) {
-  let github$;
-  if (config.misc?.github)
-    github$ = <a href={`https://github.com/${config.misc.github.user}/${config.misc.github.repo}/`} 
-                target="_blank">GitHub</a>;
-
-  let community$;
-  if (config.misc?.gitter)
-    community$ = <GitterToggle$ room={config.misc.gitter.room}/>
-
-  if (github$ && community$) return <_Footer>{github$}<hr/>{community$}</_Footer>;
-  else if (github$) return <_Footer>{github$}</_Footer>;
-  else if (community$) return <_Footer>{community$}</_Footer>;
-  else return <_Footer><Watermark/></_Footer>;
+  return <_Footer>
+  <a href="/index">Home</a>
+  <hr/>
+  <a href="http://onemiguel.es">Blog</a>
+  <hr/>
+  <a href="https://github.com/CONNECT-platform/codedoc/">Codedoc</a>
+</_Footer>;
 }
